eb1bd69e6795b4ebee7b75f814e7bae6677ae06a
albernas02 <artur.albernas2002@gmail.com>
2025-06-16 15:21:38 -0300

    finalização da aula 3

--- aulaNodeJS/src/index.js
+++ aulaNodeJS/src/index.js
@@ -1,33 +1,43 @@
 const fs = require('fs');
+const trataErros = require('./erros/funcoesErro');
 
-const caminhoArquivo = process.argv
-const link = caminhoArquivo[2]
+const caminhoArquivo = process.argv;
+const link = caminhoArquivo[2]; 
 
-fs.readFile(link, 'utf-8', (erro, texto) =>{
-    quebraParagrafos(texto);
-    console.log(texto)
+fs.readFile(link, 'utf-8', (erro, texto) => {
+  try {
+    if (erro) throw erro
+    contaPalavras(texto);
+  } catch(erro) {
+    trataErros(erro);
+  }
 })
 
-function verificaPalavrasDuplicadas(texto){
-    const listaPalavras = texto.split(' ');
-    const resultado = {}
-    listaPalavras.forEach(palavra =>{
-        if(palavra.lenght>=3){
-            const palavrasLimas = limpaPalavras()
-            resultado[palavrasLimas] = (resultado[palavra] || 0) +1;
-        }
-    })
-    return resultado
-} 
+function contaPalavras(texto) {
+  const paragrafos = extraiParagrafos(texto)
+  const contagem = paragrafos.flatMap((paragrafo) => {
+    if (!paragrafo) return [];
+    return verificaPalavrasDuplicadas(paragrafo);
+  })
+  console.log(contagem);
+}
+
+function extraiParagrafos(texto) {
+  return texto.toLowerCase().split('\n');
+}
+
 function limpaPalavras(palavra) {
   return palavra.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
 }
 
-function quebraParagrafos(texto){
-    const paragrafos = texto.toLowerCase().split('\n');
-    const contagem = paragrafos.flatMap((paragrafo) => {
-        if(!paragrafo) return [];
-    return verificaPalavrasDuplicadas(paragrafo);
-})
-    console.log(contagem)
+function verificaPalavrasDuplicadas(texto) {
+  const listaPalavras = texto.split(' ');
+  const resultado = {};
+  listaPalavras.forEach(palavra => {
+    if (palavra.length >= 3) {
+      const palavraLimpa = limpaPalavras(palavra);
+      resultado[palavraLimpa] = (resultado[palavraLimpa] || 0) + 1
+    }
+  })
+  return resultado;
 }
