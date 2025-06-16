b3a525c6e49bb8f1efe87555875cc6f190e61059
albernas02 <artur.albernas2002@gmail.com>
2025-06-16 10:19:01 -0300

    Finalização da primeira aula de objetos

---
+++ curso-objetos/alterando-objeto.js
@@ -0,0 +1,16 @@
+const estudante = {
+  nome: 'João',
+  idade: 20,
+  cpf: '123.456.789-00',
+  turma: 'A'
+};
+estudante.telefone = '(11) 98765-4321'; // Adicionando uma nova propriedade
+console.log(`Telefone: ${estudante.telefone}`);
+
+const estudante2 = {}
+estudante2.nome = 'Maria'; // Adicionando uma nova propriedade
+estudante2.idade = 22; // Adicionando uma nova propriedade
+estudante2.cpf = '987.654.321-00'; // Adicionando uma nova propriedade
+
+console.log(estudante);
+console.log(estudante2);
---
+++ curso-objetos/estrutura-objeto.js
@@ -0,0 +1,16 @@
+const listaCpfs = [
+  '123.456.789-00',
+  '987.654.321-00',
+  '111.222.333-44',
+  '555.666.777-88',]
+  const objetoPessoa = {
+    nome: 'João',
+    idade: 30,
+    cpf: '123.456.789-00',
+    turma: 'A',
+  }
+
+  console.log(`Nome: ${objetoPessoa.nome}`);
+  console.log(`Idade: ${objetoPessoa.idade}`);
+  console.log(`CPF: ${objetoPessoa.cpf}`);
+  console.log(`Turma: ${objetoPessoa.turma}`);
