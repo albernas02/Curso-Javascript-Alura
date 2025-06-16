ff80454bc30a560fba7ea55fc6bd0fce7c0f76ff
albernas02 <artur.albernas2002@gmail.com>
2025-06-16 12:40:08 -0300

    fim da aula 4

---
+++ curso-objetos/utilizarRequire.js
@@ -0,0 +1,11 @@
+const dados = require('./dados.json');
+
+console.log(dados);
+
+const produto = {
+    id : 6,
+    nome: "cueca",
+    preco: 58.99
+}
+JSON.stringify(produto)
+console.log(produto)
