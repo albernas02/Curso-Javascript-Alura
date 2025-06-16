896d97c1a4e5c043c9c83fbf5c182112033086d5
albernas02 <artur.albernas2002@gmail.com>
2025-06-16 15:09:33 -0300

    finalização da aula 2

--- aulaNodeJS/src/index.js
+++ aulaNodeJS/src/index.js
@@ -4,6 +4,30 @@ const caminhoArquivo = process.argv
 const link = caminhoArquivo[2]
 
 fs.readFile(link, 'utf-8', (erro, texto) =>{
-    console.log(texto);
-
+    quebraParagrafos(texto);
+    console.log(texto)
 })
+
+function verificaPalavrasDuplicadas(texto){
+    const listaPalavras = texto.split(' ');
+    const resultado = {}
+    listaPalavras.forEach(palavra =>{
+        if(palavra.lenght>=3){
+            const palavrasLimas = limpaPalavras()
+            resultado[palavrasLimas] = (resultado[palavra] || 0) +1;
+        }
+    })
+    return resultado
+} 
+function limpaPalavras(palavra) {
+  return palavra.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
+}
+
+function quebraParagrafos(texto){
+    const paragrafos = texto.toLowerCase().split('\n');
+    const contagem = paragrafos.flatMap((paragrafo) => {
+        if(!paragrafo) return [];
+    return verificaPalavrasDuplicadas(paragrafo);
+})
+    console.log(contagem)
+}
